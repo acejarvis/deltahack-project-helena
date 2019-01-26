02d9fc6962ebf7608b79f6fe98d56372ef7e3a2e
jamescheng40 <cheng-james@hotmail.com>
2019-01-26 16:04:00 -0500

    added example3

--- example/appengine/hello-world/standard/app.js
+++ example/appengine/hello-world/standard/app.js
@@ -34,3 +34,18 @@ app.listen(PORT, () => {
   console.log('Press Ctrl+C to quit.');
 });
 // [END gae_node_request_example]
+
+//multer
+
+var multer = require('multer');
+var upload = multer();
+
+app.post('/handleFile',upload.single('uploadCsv'), function(req, res, next) {
+  // req.file is the `uploadCsv` file 
+  // req.body will hold the text fields, if there were any 
+  console.log(req.file);
+  // the buffer here containes your file data in a byte array 
+  var csv=req.file.buffer.toString('utf8');
+});
+
+
